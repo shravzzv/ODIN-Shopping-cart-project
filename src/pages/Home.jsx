8902aca6c935afca40cfb83ee6a0464fa65027cc
sai shravan <saishravan384@gmail.com>
2024-03-06 21:36:24 +0530

    feat: Add featured section in home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,18 @@
+import Hero from '../components/Hero'
+import ProductCard from '../components/ProductCard'
 import '../styles/pages/Home.css'
+import products from '../assets/data.json'
 
 export default function Home() {
   return (
-    <main>
-      <h1>Lorem ipsum dolor sit amet.</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
-        voluptates deserunt perferendis incidunt. Dignissimos soluta maxime
-        adipisci voluptas molestiae voluptatem fugit laboriosam officiis
-        consequuntur, in autem. Molestias inventore eius beatae!
-      </p>
+    <main className='home'>
+      <Hero />
+      <h2>Featured products</h2>
+      <div className='featuredContainer'>
+        {products.slice(2, 6).map(({ id, title, images }) => (
+          <ProductCard key={id} imgSrc={images[0]} title={title} id={id} />
+        ))}
+      </div>
     </main>
   )
 }
